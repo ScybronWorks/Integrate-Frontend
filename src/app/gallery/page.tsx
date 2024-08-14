75a62d8530558828016c7afa21c2b203c6f65187
Nandakiran R <r.nandakiran@gmail.com>
2024-08-14 19:48:31 +0530

    Folder Structure (Route) for Gallery

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,8 @@
+export default function Gallery() {
+    return (
+      <main className="flex flex-col items-center justify-center p-24 font-semibold text-xl">
+        Gallery
+      </main>
+    );
+  }
+  
