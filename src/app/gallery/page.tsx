a4eaf1c164d778432ee83ed57b97fd9c9aaa50e7
maheswar-dileep <maheaswerd@gmail.com>
2024-08-15 11:10:58 +0530

    test: testing the precommit linting and styling

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,8 +1,7 @@
 export default function Gallery() {
     return (
-      <main className="flex flex-col items-center justify-center p-24 font-semibold text-xl">
-        Gallery
-      </main>
+        <main className="flex flex-col items-center justify-center p-24 font-semibold text-xl">
+            Gallery
+        </main>
     );
-  }
-  
+}
