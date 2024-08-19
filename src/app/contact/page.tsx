b91cc82d3ca54f4165b95924b01b91fc20882e89
maheswar-dileep <maheaswerd@gmail.com>
2024-08-19 10:10:38 +0530

    feat(contact-us): added contact-us page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,7 @@
 import HeaderTitle from '@/components/common/HeaderTitle';
 import Header from '@/components/layout/header/Header';
+import ClipEdgeSection from '@/components/layout/clipEdgeSection/ClipEdgeSection';
+import ContactUsCard from '@/components/molecular/contactUsCard/ContactUsCard';
 
 const ContactUs = () => {
     return (
@@ -7,6 +9,22 @@ const ContactUs = () => {
             <Header type="md">
                 <HeaderTitle title="Contact Us" />
             </Header>
+
+            <ClipEdgeSection className="flex justify-center p-12 w-full mb-0">
+                <ContactUsCard />
+            </ClipEdgeSection>
+
+            
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.8752307983627!2d76.30561067585604!3d10.027153572565224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080dafbed183bf%3A0x5951f316ba13a37e!2sLuLu%20International%20Shopping%20Mall!5e0!3m2!1sen!2sin!4v1724041891287!5m2!1sen!2sin"
+                width="100%" 
+                height="450" 
+                allowFullScreen={true}
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                style={{ border: 0 }}
+                className='mb-20'
+            ></iframe>
         </>
     );
 };
---
+++ src/components/molecular/contactUsCard/ContactUsForm.tsx
@@ -0,0 +1,49 @@
+import Button from '@/components/atomic/button/Button';
+import React from 'react';
+
+type Props = {};
+
+const ContactUsForm = (props: Props) => {
+    return (
+        <div className="w-full h-fit p-8 py-12 flex flex-col gap-4 bg-white text-black rounded-tr-lg rounded-br-[3rem]">
+            <div className="flex flex-col gap-2">
+                <h2 className="text-primary text-3xl font-bold">Enquiry Form</h2>
+                <p className="font-italianno text-lg">
+                    Submit Your Inquiry: We{`'`}ll Get Back to You with the Information You Need.
+                </p>
+            </div>
+            <div className="flex flex-col">
+                <div className="grid grid-cols-2 gap-4">
+                    <input
+                        type="text"
+                        className="bg-contactInputBg h-12 focus-visible:border-primary px-4 text-sm font-light"
+                        placeholder="Name"
+                    />
+                    <input
+                        type="text"
+                        className="bg-contactInputBg h-12 focus-visible:border-primary px-4 text-sm font-light"
+                        placeholder="Email"
+                    />
+                    <input
+                        type="number"
+                        className="bg-contactInputBg h-12 focus-visible:border-primary px-4 text-sm font-light"
+                        style={{}}
+                        placeholder="Phone"
+                    />
+                    <input
+                        type="text"
+                        className="bg-contactInputBg h-12 focus-visible:border-primary px-4 text-sm font-light"
+                        placeholder="Subject"
+                    />
+                    <textarea
+                        rows={4}
+                        className="col-span-2 bg-contactInputBg focus-visible:border-primary p-4 text-sm font-light"
+                    />
+                </div>
+                <Button className='w-fit mt-4'>Submit Now</Button>
+            </div>
+        </div>
+    );
+};
+
+export default ContactUsForm;
